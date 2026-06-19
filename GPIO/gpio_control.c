b5acef4bdc65b4952166cb2292adb616815730a6
Jared Hagemann <jdx11088@gmail.com>
2026-06-19 12:54:22 -0500

    Cleaned up gpio_control.c

--- GPIO/gpio_control.c
+++ GPIO/gpio_control.c
@@ -8,31 +8,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <gpiod.h>
 
-#ifndef	CONSUMER
-#define	CONSUMER	"Consumer"
-#endif
 
-int main(int argc, char*argv[]){
-	unsigned int status = 0;
+#define	CONSUMER	"Consumer"
+
+
+int main(int argc, char*argv[]){	
 	if(argc != 2){
-		perror("Must have only one argument.  Options are: On Off\n");
+		fprintf(stderr,"Must have only one argument.  Options are: On Off\n");
 		return 1;
 	}
+	
+	unsigned int status = 0;
+	if(strcmp(argv[1], "On") == 0){
+		status = 1;
+	}
+	else if(strcmp(argv[1], "Off") == 0){
+		status = 0;
+	}
 	else{
-		if(strcmp(argv[1], "On") == 0){
-			status = 1;
-		}
-		else if(strcmp(argv[1], "Off") == 0){
-			status = 0;
-		}
-		else{
-			perror("Invalid argument.  Must be either On or Off.\n");
-			return 1;
-		}
+		fprintf(stderr, "Invalid argument.  Must be either On or Off.\n");
+		return 1;
 	}
 	
 	char *chipname = "gpiochip0";
@@ -44,29 +41,28 @@ int main(int argc, char*argv[]){
 	
 	chip = gpiod_chip_open_by_name(chipname);
 	if (!chip) {
-		perror("Open chip failed\n");
+		fprintf(stderr, "Open chip failed\n");
 		return 1;
 	}
 	line = gpiod_chip_get_line(chip, line_num);
 	if (!line) {
-		perror("Get line failed\n");
+		fprintf(stderr, "Get line failed\n");
+		gpiod_chip_close(chip);
 		return 1;
 	}
 	
 	ret = gpiod_line_request_output(line, CONSUMER, status);
 	if (ret < 0) {
-		perror("Request line as output failed\n");
+		fprintf(stderr, "Request line as output failed\n");
+		gpiod_line_release(line);
+		gpiod_chip_close(chip);
 		return 1;
 	}
 	
 	//Do Something
-	ret = gpiod_line_set_value(line, status);
+	gpiod_line_set_value(line, status);	
 	
-	
-	release_line:
-		gpiod_line_release(line);
-	close_chip:
-		gpiod_chip_close(chip);
-	end:
+	gpiod_line_release(line);
+	gpiod_chip_close(chip);
 	return 0;
 }
